12f6903f88b1a4af2fcf464da57703134fa8398d
david-why <david_why@outlook.com>
2016-09-12 20:58:22 +0800

    Update math-1.py

--- math-1.py
+++ math-1.py
@@ -6,17 +6,12 @@ n = input('Please input the second number: ')
 mn = m
 nn = n
 
-if n < m:
-  i = n
-  n = m
-  m = i
-
 while True:
   i = n % m
   if i == 0:
-    print '%i is GCD.'%m
     break
   n = m
   m = i
-
-print '%i is LCM.'%(nm * nn / m)
+  
+print '%i is GCD.'%m
+print '%i is LCM.'%(mn * nn / m)
